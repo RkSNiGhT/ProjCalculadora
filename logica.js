073b13a451dfd44309148ad5793094464bc39dcb
ric <ricgames1@hotmail.com>
2024-10-23 07:29:25 -0300

    closes #3 - Primeira op done

---
+++ logica.js
@@ -0,0 +1,9 @@
+function calcSoma(){
+    var x = parseFloat( document.getElementById("N1").value) ;
+    var y = parseFloat( document.getElementById("N2").value );
+
+    var r = x+y;
+
+    document.getElementById("Resultado").value = r;
+
+}
