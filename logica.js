64f93b1a0af52c81ad0a00132ada2d9585fe48ca
ric <ricgames1@hotmail.com>
2024-10-23 09:57:57 -0300

    closes #5, closes #6 - hitorico e debug

--- logica.js
+++ logica.js
@@ -2,30 +2,42 @@ function calcSoma(button){
     var x = parseFloat( document.getElementById("N1").value) ;
     var y = parseFloat( document.getElementById("N2").value );
 
+    if(Number.isNaN(x) == true){
+        x=0;
+    }
+    if(Number.isNaN(y) == true){
+        y=0;
+    }
 
-    var op = button.id;
 
-    if(op == "btnSomar"){
+    var op = button.textContent;
+
+    if(op == "+"){
 
         var r = x+y;
 
         document.getElementById("Resultado").value = r;
-    }else if(op=="btnSub"){
+    }else if(op=="-"){
         var r = x-y;
 
         document.getElementById("Resultado").value = r;
-    }
-    if(op == "btnDiv"){
+    }else if(op == "/"){
 
         var r = x/y;
 
         document.getElementById("Resultado").value = r;
-    }else if(op=="btnMulti"){
+    }else if(op=="*"){
         var r = x*y;
 
         document.getElementById("Resultado").value = r;
     }
 
+    document.getElementById("historico").innerHTML = "<p>" + x + op + y + " = " + r + "</p>" + document.getElementById("historico").innerHTML;
 
+    if(document.getElementById("historico").children.length>10){
 
+        document.getElementById("historico").removeChild(document.getElementById("historico").childNodes[10]);
+
+    }
+    
 }
